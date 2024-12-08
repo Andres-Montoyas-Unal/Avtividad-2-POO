8354b4c56f1dafacd81288adadc5ba2516be7ce8
Andres-Montoyas-Unal <anmontoyas@unal.edu.co>
2024-12-08 09:01:29 -0500

    Create elercicio_pro_21.py

---
+++ elercicio_pro_21.py
@@ -0,0 +1,7 @@
+a=int(input("Ingrese el valor del lado a: "))
+b=int(input("Ingrese el valor del lado b: "))
+c=int(input("Ingrese el valor del lado c: "))
+perimetro=a+b+c
+semiperimetro=perimetro/2
+area=(s*(s-a)*(s-b)*(s-c))**0.5
+print(f"Perimetro= {perimetro}, semiperimetro= {semiperimetro}, area= {area}")
