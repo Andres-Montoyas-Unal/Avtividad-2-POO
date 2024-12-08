b0f3d99623a90d46b12bc9ca3fc3d38bda7ec883
Andres-Montoyas-Unal <anmontoyas@unal.edu.co>
2024-12-08 08:50:42 -0500

    Create ejercicio_pro_19.py

---
+++ ejercicio_pro_19.py
@@ -0,0 +1,5 @@
+lado=float(input("digite el lado del triangulo equilatero: "))
+perimetro=3*lado
+altura=(lado*(3)**0.5)/2
+area=(lado**2 * (3)**0.5)/4
+print(f"Perimetro= {perimetro}, altura= {altura}, area= {area}")
