4138f424c6d32d0491d978084370de64722861b8
Andres-Montoyas-Unal <anmontoyas@unal.edu.co>
2024-12-08 08:41:08 -0500

    Update ejercicio_pro_18.py

--- ejercicio_pro_18.py
+++ ejercicio_pro_18.py
@@ -2,7 +2,7 @@ codigo=int(input("Ingrese el codigo del Empleado: "))
 nombres=input("Ingrese el nombre del Empleado: ")
 horas_trbajadas=int(input("Ingrese las horas trabajadas por el empleado: "))
 valor_hora=float(input("Ingrese el valor de la hora: "))
-porcentaje=int(input("Ingrese el porcentaje de retemcion en la fuente: "))
+porcentaje=float(input("Ingrese el porcentaje de retemcion en la fuente: "))
 
 salario_bruto=horas_trbajadas*valor_hora
 salario_neto=salario_bruto-(salario_bruto*(porcentaje/100))
